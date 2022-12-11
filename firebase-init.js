9de03be886af7c8fa3b7ff2ee47218af9a08e4f1
Júlia Gomes <gomesjulia621@gmail.com>
2022-12-11 01:58:32 -0300

    add configs

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD79-5E0pu9ztdLr0I2D1YxxZs5tVAY1WI',
+  authDomain: 'controle-de-gastos-2a5d8.firebaseapp.com',
+  projectId: 'controle-de-gastos-2a5d8',
+  storageBucket: 'controle-de-gastos-2a5d8.appspot.com',
+  messagingSenderId: '845553504713',
+  appId: '1:845553504713:web:a0d2ac74d5ee9dac36d818'
+}
+firebase.initializeApp(firebaseConfig)
